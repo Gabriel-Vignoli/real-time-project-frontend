537b19c04ec231f4175909934cb98467a746639b
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-07-27 15:26:19 -0300

    Instalação e implementação do websocket no frontend

--- src/components/TopicRoom.tsx
+++ src/components/TopicRoom.tsx
@@ -1,12 +1,78 @@
-import { Dispatch, SetStateAction } from "react"
-import { Topic } from "./Home"
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Topic } from "./Home";
+import { io } from "socket.io-client";
+import { User, UserContext } from "../App";
+import MessageBox from "./MessageBox";
 
 type TopicRoomProps = {
-  topic: Topic
-  setOpenTopic: Dispatch<SetStateAction<Topic | null>>
-}
+  topic: Topic;
+  setOpenTopic: Dispatch<SetStateAction<Topic | null>>;
+};
+
+export type Message = {
+  _id: string;
+  content: string;
+  author?: User;
+  createdAt: string;
+};
+
+const socket = io("http://localhost:3000");
 
 export default function TopicRoom({ topic, setOpenTopic }: TopicRoomProps) {
+  const { user } = useContext(UserContext);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    loadMessages();
+  }, [topic]);
+
+  useEffect(() => {
+    if (user && topic) {
+      socket.emit("join_room", { name: user.name, topicId: topic._id });
+
+      socket.on("new_message", (newMessage: Message) => {
+        setMessages((mostRecentState) => [...mostRecentState, newMessage]);
+      });
+
+      return () => {
+        socket.emit("leave_room", { name: user.name, topicId: topic._id });
+        socket.off("new_message");
+      };
+    }
+  }, [user, topic]);
+
+  async function loadMessages() {
+    try {
+      const response = await fetch(`http://localhost:3000/topics/${topic._id}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setMessages(data.messages);
+    } catch (error) {
+      console.error("Failed to load messages:", error);
+    }
+  }
+
+  async function handleSubmit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    const content = formData.get("message")?.toString();
+    if (!content || !user || !topic) return;
+
+    ev.currentTarget.reset();
+
+    socket.emit("send_message", { content, author: user, topicId: topic._id });
+  }
+
   return (
     <main className="room">
       <header>
@@ -15,12 +81,15 @@ export default function TopicRoom({ topic, setOpenTopic }: TopicRoomProps) {
       </header>
 
       <section className="messages">
+        {messages.map((message) => (
+          <MessageBox message={message}></MessageBox>
+        ))}
       </section>
 
-      <form className="send-message-form inline-form">
-        <input type="text" name="message" id="message" />
+      <form className="send-message-form inline-form" onSubmit={handleSubmit}>
+        <input type="text" name="message" id="message" required />
         <button>Enviar</button>
       </form>
     </main>
-  )
+  );
 }
